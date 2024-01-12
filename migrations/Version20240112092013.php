3b97eddc783b1d4cafa0ac5039668d8d87667eba
John Joseph CEPPIC <john.josephpro@gmail.com>
2024-01-12 10:20:52 +0100

    Migration

--- migrations/Version20240112092013.php
+++ migrations/Version20240112092013.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240111152622 extends AbstractMigration
+final class Version20240112092013 extends AbstractMigration
 {
     public function getDescription(): string
     {
